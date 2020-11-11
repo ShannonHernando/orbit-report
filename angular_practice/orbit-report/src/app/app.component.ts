a9a905c6f6412cd635209c1727b52427da8ef28b
Shannon Hernando <shannon.hernando@gmail.com>
2020-11-10 18:10:21 -0600

    final

--- angular_practice/orbit-report/src/app/app.component.ts
+++ angular_practice/orbit-report/src/app/app.component.ts
@@ -10,10 +10,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite [];
   satellitesUrl:string = 'https://handlers.education.launchcode.org/static/satellites.json';
 
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
 
     window.fetch(this.satellitesUrl).then(function(response) {
        response.json().then(function(data) {
@@ -23,11 +25,23 @@ export class AppComponent {
             let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
             this.sourceList.push(satellite);
           }
-        
+          this.displayList = this.sourceList.slice(0);
           
        }.bind(this));
     }.bind(this));
  
  }
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  
+  this.displayList = matchingSatellites;
+}
  }
 
